c80f16a42892528129aadeee8d3b6a7c4518471b
katanazero86 <katanazero86@gmail.com>
2022-04-25 01:45:44 +0900

    [설정 추가] - react-query 적용(@zero86/react-clone)

--- packages/react-clone/src/main.tsx
+++ packages/react-clone/src/main.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import App from './App';
 import './index.css';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
