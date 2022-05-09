bd383e4f2872e8de67dcb5221a8f3244c28288eb
zero86 <katanazero86@gmail.com>
2022-05-09 22:06:26 +0900

    [기타] tsc 설정 변경 및 HMR 이슈 처리

--- packages/components/src/components/Tabs/Tab/TabComponent.d.ts
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-interface TabComponentProps {
-    name: string;
-    value: number | string;
-    currentValue?: number | string;
-    handleClick?(e: React.SyntheticEvent, newValue: number | string): void;
-}
-export default function TabComponent({ name, value, currentValue, handleClick }: TabComponentProps): JSX.Element;
-export {};
